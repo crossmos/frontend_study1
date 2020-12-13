33aa835f8fdfa84a5f9d55a80fa89dcef42638ad
Sergey <cross9494@mail.ru>
2020-12-13 21:13:58 +0300

    qqq

--- Вёрстка макета из Figma1/js/main.js
+++ Вёрстка макета из Figma1/js/main.js
@@ -1,10 +1,14 @@
 (function () {
     const header = document.querySelector('.header'); 
     const top = document.querySelector('.header__wrapper')
+
     window.onscroll = () => {
         if (window.pageYOffset > 50) {
             header.classList.add('header_active');
             top.classList.add('header__wrapper_active');
+        }   else {
+            header.classList.remove('header_active');
+            top.classList.remove('header__wrapper_active');
         }
     };
 }());
