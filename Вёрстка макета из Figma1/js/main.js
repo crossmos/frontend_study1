9ecabdce18796e30a9b4a6bafaa6570e0387cd6c
Serega <cross9494@mail.ru>
2020-12-14 14:42:55 +0300

    qqq

--- Вёрстка макета из Figma1/js/main.js
+++ Вёрстка макета из Figma1/js/main.js
@@ -1,14 +1,27 @@
 (function () {
-    const header = document.querySelector('.header'); 
+    const header = document.querySelector('.header');
     const top = document.querySelector('.header__wrapper')
 
     window.onscroll = () => {
         if (window.pageYOffset > 50) {
             header.classList.add('header_active');
             top.classList.add('header__wrapper_active');
-        }   else {
+        } else {
             header.classList.remove('header_active');
             top.classList.remove('header__wrapper_active');
         }
     };
+}());
+
+
+
+// Burger
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav-active');
+    });
 }());
